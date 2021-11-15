ccad1bc1da144702343df3d99eec850db8fb5329
Phil Mui <thephilmui@gmail.com>
2021-11-14 22:02:30 -0800

    Initial Commit

---
+++ Safe Cargo Delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Safe Cargo Delivery
+//
+//  Created by Phil Mui on 11/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
